a553a6fdd959bd058e9d8367d5489ae8f9a13906
Đoàn Văn Điệp <147433897+doanvandiep@users.noreply.github.com>
2024-03-11 17:36:33 +0700

    Mã gray 003

--- CTDL009.cpp
+++ CTDL009.cpp
@@ -1,31 +1,48 @@
-#include<bits/stdc++.h>
-using namespace std;
-void check()
+#include<iostream>
+
+/*
+step1:
+s = 01101
+     |
+res= 01101
+
+0!=1 => res[1]=1;
+step2:
+s = 01101
+      |
+res= 01101
+1=1 => res[2]=0;
+step3:
+s = 01101
+       |
+res= 01001
+0=0 => res[3]=0;
+step4;
+s = 01101
+        |
+res= 01001
+1!=0 => res[4]=1;
+*/
+
+void check(std::string s)
 {
-	string s;
-	cin >> s;
-	string res=s;
+	std::string res=s;
 	for(int i=1;i<s.size();i++)
 	{
-		if((res[i-1]-'0')^0==(s[i]-'0')) res[i]='0';//XOR với số 0 ra chính số đó
-		// vi du:
-		/*
-		01101
-		01101
-		res[1]=1=s[2];
-		=> res[2]=0 để res[1]^res[2]=0^1=1=s[2];
-		*/
+		if((res[i-1]-'0')^0==(s[i]-'0')) res[i]='0';
 		else res[i]='1';
 	}
-	cout << res;
+	std::cout << res << std::endl;
 }
+
 int main()
 {
 	int t;
-	cin >> t;
+	std::cin >> t;
 	while(t--)
 	{
-		check();
-		if(t>0) cout << endl;
-	} 
+		std::string s;
+		std::cin >> s;
+		check(s);	
+	}
 }
